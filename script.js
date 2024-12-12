8cf6455c0adefbd607c53928ecbbaeb3e5a2a6f2
mankuShona <kamrul.islam3105@gmail.com>
2024-12-13 05:11:23 +0600

    breifJS a1a2a3

--- script.js
+++ script.js
@@ -17,3 +17,42 @@ type of e = ${typeof e}<br>
 type of b = ${typeof b}<br>
 type of a = ${typeof a}<br>
 type of f = ${typeof f}<br>`;
+
+function conditionals(){
+    let greetings;
+    let time = new Date().getHours();
+    if (time>=6 && time < 12){
+        greetings = "Good Morning";
+        }
+    else if(time === 12){
+        greetings = "Good Noon";
+    }
+    else if(time > 12 && time < 17){
+        greetings = "Good Afternoon";
+    }
+    else if ( time >= 17 && time < 19){
+        greetings = "Good Evening";
+
+    }
+    else{
+        greetings = "Good Night";
+    };
+
+    document.getElementById("hour").innerHTML = 
+    `
+    Current Hour is : ${time}
+    `;
+    document.getElementById("greet").innerHTML = greetings;
+
+};
+
+function while_loop(){
+    let text = ``;
+    let i = 0;
+    while(i<10){
+        text += `<br> The number is: ${i}`;
+        i++;
+    };
+    document.getElementById("whileLoop").innerHTML = text;
+}
+
